297a5f325c5f72bbe0085b2b75e86eb4ad8779f3
0xab42 <0xab42@gmail.com>
2024-02-26 01:46:21 +0700

    add dog.go

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "when the puppy grows up is says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/0xab42/dog
+
+go 1.22.0
